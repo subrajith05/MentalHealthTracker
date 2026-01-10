368f6bd99dfd91168d6a2d069c1d647e1520ce9b
subrajith05 <subrajith.r@gmail.com>
2026-01-10 12:16:03 +0530

    Modified requirements to match bcrypt version

--- backend/appfiles/hashing.py
+++ backend/appfiles/hashing.py
@@ -5,10 +5,8 @@ pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
 class Hash:
     #Utility to encrypt the password
     def bcrypt(password):
-        safe_password = password.encode("utf-8")[:72]
-        return pwd_cxt.hash(safe_password)
+        return pwd_cxt.hash(password)
     
     #Uitility to verify the password
     def verify_password(plain_password, hashed_password):
-        safe_password = plain_password.encode("utf-8")[:72]
-        return pwd_cxt.verify(safe_password, hashed_password)
+        return pwd_cxt.verify(plain_password, hashed_password)
