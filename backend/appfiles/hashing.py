7952b619f6362c0860d7bc7df3746236d0e87b52
subrajith05 <subrajith.r@gmail.com>
2026-01-10 12:07:25 +0530

    Fixed password issues

--- backend/appfiles/hashing.py
+++ backend/appfiles/hashing.py
@@ -5,8 +5,10 @@ pwd_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
 class Hash:
     #Utility to encrypt the password
     def bcrypt(password):
-        return pwd_cxt.hash(password)
+        safe_password = password.encode("utf-8")[:72]
+        return pwd_cxt.hash(safe_password)
     
     #Uitility to verify the password
     def verify_password(plain_password, hashed_password):
-        return pwd_cxt.verify(plain_password, hashed_password)
+        safe_password = plain_password.encode("utf-8")[:72]
+        return pwd_cxt.verify(safe_password, hashed_password)
